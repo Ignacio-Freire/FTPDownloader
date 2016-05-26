61275c26beca06c698633e611d193a94ae981cce
RealTkwk <nachofreire@gmail.com>
2016-05-26 16:31:20 -0300

    Opción para descargar archivos sueltos.

--- Bajar evidencias.py
+++ Bajar evidencias.py
@@ -3,7 +3,6 @@ import shutil
 import time
 from ftplib import FTP
 
-
 start = time.time()
 
 user = ''
@@ -12,27 +11,19 @@ IP = ''
 
 req = ''
 
-tests = ['']
+tests = []
 
-filenames = [('', '', True)]
+filelist = [('', '', True)]
 
 ftp = FTP(IP)
 ftp.login(user, passwd=passw)
 
 
-def writeline(line):
-    file.write(line + "\n")
+def download_files(filesnames):
+    def writeline(line):
+        file.write(line + "\n")
 
-try:
-    os.makedirs(os.path.expanduser('~/Desktop/PPU {}'.format(req)))
-except FileExistsError:
-    shutil.rmtree(os.path.expanduser('~/Desktop/PPU {}'.format(req)))
-    os.makedirs(os.path.expanduser('~/Desktop/PPU {}'.format(req)))
-
-for test, name in enumerate(tests):
-    folder = os.path.expanduser('~/Desktop/PPU {}/Caso {} - {}/'.format(req, test + 1, name))
-    os.makedirs(folder)
-    for files in filenames:
+    for files in filesnames:
 
         if files[0] == '':
             file = open(folder + files[1] + '.txt', 'w')
@@ -49,6 +40,23 @@ for test, name in enumerate(tests):
         except:
             print('Archivo {} no encontrado.'.format(retrieve))
 
+
+carpeta = 'Archivos descargados' if req == '' else 'PPU {}'.format(req)
+
+try:
+    os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+except FileExistsError:
+    shutil.rmtree(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+    os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+
+if tests:
+    for test, name in enumerate(tests):
+        folder = os.path.expanduser('~/Desktop/{}/Caso {} - {}/'.format(carpeta, test + 1, name))
+        os.makedirs(folder)
+        download_files(filelist)
+else:
+    download_files(filelist)
+
 ftp.quit()
 
-print('Tiempo: {:.2f}'.format(time.time()-start))
+print('Tiempo: {:.2f}'.format(time.time() - start))
