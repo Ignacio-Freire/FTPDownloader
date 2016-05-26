bd6f1477f13b0d0dba352362b30cf4df6b7626db
RealTkwk <nachofreire@gmail.com>
2016-05-26 13:54:17 -0300

    Se agregó opción para bajar archivos generales.

--- Bajar evidencias.py
+++ Bajar evidencias.py
@@ -14,10 +14,12 @@ req = ''
 
 tests = ['']
 
-filenames = [('', '')]
+filenames = [('', '', True)]
 
 ftp = FTP(IP)
 ftp.login(user, passwd=passw)
+
+addtest = True
 
 
 def writeline(line):
@@ -33,8 +35,17 @@ for test, name in enumerate(tests):
     folder = 'PPU {}\Caso {} - {}\\'.format(req, test + 1, name)
     os.makedirs(folder)
     for files in filenames:
-        file = open(folder + files[0] + ' - {}.txt'.format(name), 'w')
-        retrieve = files[1] + '.T{}'.format(test + 1)
+
+        if files[0] == '':
+            file = open(folder + files[1] + 'txt', 'w')
+        else:
+            file = open(folder + files[0] + ' - {}.txt'.format(name), 'w')
+
+        if files[2]:
+            retrieve = files[1] + '.T{}'.format(test + 1)
+        else:
+            retrieve = files[1]
+
         try:
             ftp.retrlines("RETR '{}'".format(retrieve), writeline)
         except:
