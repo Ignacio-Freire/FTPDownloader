24391916485ae13a7b447c2272a87566081651c6
RealTkwk <nachofreire@gmail.com>
2016-05-27 15:58:50 -0300

    Se creo una interfaz gráfica.

--- Bajar evidencias.py
+++ Bajar evidencias.py
@@ -1,73 +1,216 @@
 # ------------------------------------------------Ignacio Freire-------------------------------------------------------#
 import os
 import shutil
+import sys
 import time
 from ftplib import FTP
 
-start = time.time()
-
-user = ''
-passw = ''
-IP = ''
-
-req = ''
-
-tests = []
-
-filelist = [('', '', True)]
-
-ftp = FTP(IP)
-ftp.login(user, passwd=passw)
+import gui
 
 
-def download_files(filesnames, location, **kwargs):
-    names = kwargs.get('name', '')
-    caso = kwargs.get('caso', 1)
+def comenzar_descarga():
+    start = time.time()
 
-    def writeline(line):
-        file.write(line + "\n")
+    user = ui.lineUser.text()
+    passw = ui.linePass.text()
+    IP = ui.lineIP.text()
 
-    for files in filesnames:
+    req = ui.lineReq.text()
 
-        if files[0] == '':
-            filename = location + files[1] + '.txt'
-        elif tests:
-            filename = location + files[0] + ' - {}.txt'.format(names)
-        else:
-            filename = location + files[0] + '.txt'
+    if IP == '' or user == '' or passw == '':
+        print('Datos de conexion incorrectos o faltantes.')
+    else:
 
-        file = open(filename, 'w')
+        ftp = FTP(IP)
+        ftp.login(user, passwd=passw)
 
-        if files[2]:
-            retrieve = files[1] + '.T{}'.format(caso)
-        else:
-            retrieve = files[1]
+        carpeta = 'Archivos descargados' if req == '' else 'PPU {}'.format(req)
+
+        def download_files(filesnames, location, **kwargs):
+            names = kwargs.get('name', '')
+            caso = kwargs.get('caso', 1)
+
+            def writeline(line):
+                file.write(line + "\n")
+
+            for files in filesnames:
+
+                if files[0] == '':
+                    filename = location + files[1] + '.txt'
+                elif tests:
+                    filename = location + files[0] + ' - {}.txt'.format(names)
+                else:
+                    filename = location + files[0] + '.txt'
+
+                file = open(filename, 'w')
+
+                if files[2]:
+                    retrieve = files[1] + '.T{}'.format(caso)
+                else:
+                    retrieve = files[1]
+
+                try:
+                    ftp.retrlines("RETR '{}'".format(retrieve), writeline)
+                except:
+                    file.close()
+                    print('Archivo {} no encontrado.'.format(retrieve))
+                    os.remove(filename)
 
         try:
-            ftp.retrlines("RETR '{}'".format(retrieve), writeline)
-        except:
-            file.close()
-            print('Archivo {} no encontrado.'.format(retrieve))
-            os.remove(filename)
+            os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+        except FileExistsError:
+            shutil.rmtree(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+            os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+
+        if tests:
+            for test, name in enumerate(tests):
+                folder = os.path.expanduser('~/Desktop/{}/Caso {} - {}/'.format(carpeta, test + 1, name))
+                os.makedirs(folder)
+                download_files(filelist, folder, name=name, caso=test + 1)
+        else:
+            folder = os.path.expanduser('~/Desktop/{}/'.format(carpeta))
+            download_files(filelist, folder)
+
+        ftp.quit()
+        print('Tiempo: {:.2f}'.format(time.time() - start))
 
 
-carpeta = 'Archivos descargados' if req == '' else 'PPU {}'.format(req)
+def add_caso():
+    test = ui.lineCaso.text()
 
-try:
-    os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
-except FileExistsError:
-    shutil.rmtree(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
-    os.makedirs(os.path.expanduser('~/Desktop/{}'.format(carpeta)))
+    if test == '':
+        print('Campo vacio.')
+    else:
+        tests.append(test)
+        ui.lineCaso.clear()
+        load_casos()
 
-if tests:
-    for test, name in enumerate(tests):
-        folder = os.path.expanduser('~/Desktop/{}/Caso {} - {}/'.format(carpeta, test + 1, name))
-        os.makedirs(folder)
-        download_files(filelist, folder, name=name, caso=test + 1)
-else:
-    folder = os.path.expanduser('~/Desktop/{}/'.format(carpeta))
-    download_files(filelist, folder)
 
-ftp.quit()
+def add_archivo():
+    mainframe = ui.lineNombreMainframe.text()
+    nombre = ui.lineNombre.text()
+    tx = True if ui.checkTx.isChecked() else False
 
-print('Tiempo: {:.2f}'.format(time.time() - start))
+    if mainframe == '':
+        print('Nombre en Mainframe vacio.')
+    else:
+        if nombre == '':
+            print('Nombre vacio. Se nombrara como archivo en mainframe.')
+        filelist.append((nombre, mainframe, tx))
+        ui.lineNombreMainframe.clear()
+        ui.lineNombre.clear()
+        load_archivos()
+
+
+def load_casos():
+    ui.listPruebas.clear()
+    for caso in tests:
+        ui.listPruebas.addItem(caso.capitalize())
+
+
+def load_archivos():
+    ui.listArchivos.clear()
+    for item in filelist:
+        ui.listArchivos.addItem(
+            '{} - {}   - {}'.format(item[0].capitalize(), item[1].upper(), 'Tx' if item[2] == True else 'Único'))
+
+
+def clear_pruebas():
+    tests.clear()
+    ui.listPruebas.clear()
+    load_casos()
+
+
+def clear_archivos():
+    filelist.clear()
+    ui.listArchivos.clear()
+    load_archivos()
+
+
+def borrar_caso():
+    index = ui.listPruebas.currentRow()
+    if index == -1:
+        print('Seleccionar item.')
+    else:
+        tests.pop()
+        load_casos()
+
+
+def borrar_archivo():
+    index = ui.listArchivos.currentRow()
+    if index == -1:
+        print('Seleccionar item.')
+    else:
+        filelist.pop(index)
+        load_archivos()
+
+
+def renombrar_caso():
+    new_name = ui.lineCaso.text()
+    index = ui.listPruebas.currentRow()
+
+    if new_name == '':
+        print('Ingresar nombre nuevo')
+    elif index == -1:
+        print('Seleccionar item.')
+    else:
+        tests[index] = new_name
+        ui.lineCaso.clear()
+        load_casos()
+
+
+def renombrar_archivo():
+    index = ui.listArchivos.currentRow()
+
+    new_name = ui.lineNombre.text()
+    new_mainframe_name = ui.lineNombreMainframe.text()
+    new_state = True if ui.checkTx.isChecked() else False
+
+    if new_mainframe_name == '':
+        print('Ingresar nombre nuevo.')
+    elif index == -1:
+        print('Seleccionar item.')
+    else:
+        filelist[index] = (new_name, new_mainframe_name, new_state)
+        ui.lineNombre.clear()
+        ui.lineNombreMainframe.clear()
+        load_archivos()
+
+
+def reset_all():
+    clear_archivos()
+    clear_pruebas()
+    ui.lineReq.clear()
+    ui.lineCaso.clear()
+    ui.lineNombreMainframe.clear()
+    ui.lineNombre.clear()
+    ui.checkTx.setCheckState(0)
+
+
+if __name__ == "__main__":
+    app = gui.QtWidgets.QApplication(sys.argv)
+    MainWindow = gui.QtWidgets.QMainWindow()
+    ui = gui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    tests = []
+    filelist = []
+
+    load_casos()
+    load_archivos()
+
+    ui.pushAddPrueba.clicked.connect(add_caso)
+    ui.pushAddArchivo.clicked.connect(add_archivo)
+    ui.pushClearArchivos.clicked.connect(clear_archivos)
+    ui.pushClearPruebas.clicked.connect(clear_pruebas)
+    ui.pushDeletePrueba.clicked.connect(borrar_caso)
+    ui.pushDeleteArchivo.clicked.connect(borrar_archivo)
+    ui.pushRenamePrueba.clicked.connect(renombrar_caso)
+    ui.pushRenameArchivo.clicked.connect(renombrar_archivo)
+
+    ui.pushDownload.clicked.connect(comenzar_descarga)
+
+    ui.pushClearAll.clicked.connect(reset_all)
+
+    sys.exit(app.exec_())
